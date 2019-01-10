f72b8d02c2ab5401464caf21f7713ef1f96d00ff
Chengyue Gong <631019103@qq.com>
2019-01-09 22:51:10 -0500

    Initial Commit

---
+++ MovieSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by Chengyue Gong on 2019/1/9.
+//  Copyright © 2019 Chengyue Gong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
